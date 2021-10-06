ef13ba2a1010d7489ac4aec1ab3ebb1857134fb6
NatalyMora21 <tatamora21gmail.com>
2021-10-05 21:02:11 -0500

    Llamada a funciones con valores predeterminados

---
+++ main.js
@@ -0,0 +1,11 @@
+const  suma =  require ('./operaciones/suma.js');
+const resta = require ('./operaciones/resta.js');
+const multiplicacion = require ('./operaciones/multiplicacion.js');
+const division = require( "./operaciones/division.js")
+
+console.log(suma(1,4));
+console.log(resta(1,4))
+console.log(multiplicacion(-10,4));
+console.log(division(2,-10))
+
+
